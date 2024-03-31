5aa8bd2ade06b912735952ec1738c3ff0dd3dabf
ZRMYDYCG <547471919@qq.com>
2024-03-31 20:17:40 +0800

    🎉 配置 scss 全局变量

--- vite.config.js
+++ vite.config.js
@@ -27,6 +27,16 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+    css: {
+      // css预处理器
+      preprocessorOptions: {
+        scss: {
+          // 引入 variable.scss 这样就可以在全局中使用 variable.scss 中预定义的变量了
+          // 注意：给导入的路径最后加上 `;`
+          additionalData: '@import "@/assets/css/variable.scss";'
+        }
+      }
     }
   }
 })
