c9f2c944185f2706e518007762012e6f4b66331e
ZRMYDYCG <547471919@qq.com>
2024-03-31 20:36:03 +0800

    🎉 解决 eslint、prettier 冲突问题

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,7 +6,10 @@ module.exports = {
   'extends': [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
-    '@vue/eslint-config-prettier/skip-formatting'
+    '@vue/eslint-config-prettier/skip-formatting',
+    // 解决 eslint、prettier 冲突问题
+    'plugin:prettier/recommended',
+    'eslint-config-prettier'
   ],
   parserOptions: {
     ecmaVersion: 'latest'
