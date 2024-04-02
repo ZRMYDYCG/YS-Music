4c8e605ef946febe182d1ea2c29ce8a36d301201
ZRMYDYCG <547471919@qq.com>
2024-04-02 13:03:20 +0800

    🔆 feat-layout 全局注册 element-plus 图标

--- src/layout/Layout.vue
+++ src/layout/Layout.vue
@@ -3,7 +3,9 @@
     <el-container>
       <el-aside width="200px">Aside</el-aside>
       <el-container>
-        <el-header>Header</el-header>
+        <el-header>
+          <layout-header></layout-header>
+        </el-header>
         <el-main>
           <router-view></router-view>
         </el-main>
@@ -12,6 +14,8 @@
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import LayoutHeader from '@/layout/components/LayoutHeader/src/LayoutHeader.vue'
+</script>
 
 <style lang="scss"></style>
--- src/main.js
+++ src/main.js
@@ -2,9 +2,20 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
-import 'normalize.css' // 添置样式重置库
+import 'normalize.css' // 重置样式
 import './assets/scss/index.scss' // 重要样式出口
+import { registerElementPlusIcons } from './utils/registerElementPlusIcons'
+
 const app = createApp(App)
+
+// 使用Pinia状态管理库
 app.use(createPinia())
+
+// 使用路由
 app.use(router)
+
+// 注册Element Plus图标
+registerElementPlusIcons(app)
+
+// 挂载应用
 app.mount('#app')
